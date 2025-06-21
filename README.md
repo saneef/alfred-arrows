e4e5611efe19912497535f7442a1aee32012b22a
Saneef Ansari <hello@saneef.com>
2025-06-21 09:12:29 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Alfred Arrows
+
+You can search for the Unicode arrow characters from Alfred.
+This workflow inspired from the Trys Mudford's [Arrows](https://arrows.trysmudford.com) webpage.
+
+## Requirements
+
+- [Alfred](https://alfred.app)
+- Ruby
+
+## Installing
+
+1. Download the latest release ZIP from releases.
+2. Unzip.
+3. Open `Unravel.alfredworkflow` to install the workflow.
+
+## Usage
+
+In Alfred, use keyword `arr` to search for arrow characters. Example: `arr top`
---
+++ arrow_search.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+
+require 'json'
+
+# Space separated keywords in Alfred is passed as a single argument
+queries = ARGV.empty? ? ARGV : ARGV[0].split(' ')
+
+arrows = [
+	{ keywords: "top", char: "↑" },
+	{ keywords: "top right", char: "↗" },
+	{ keywords: "right", char: "→" },
+	{ keywords: "bottom right", char: "↘" },
+	{ keywords: "bottom", char: "↓" },
+	{ keywords: "bottom left", char: "↙" },
+	{ keywords: "left", char: "←" },
+	{ keywords: "top left", char: "↖" },
+]
+
+def search_arrows(arrows, queries)
+	if queries.empty?
+		arrows
+	else
+		queries.reduce(arrows) do |acc, q|
+			acc.filter { |arrow| arrow[:keywords].include?(q) }
+		end
+	end
+end
+
+def generate_alfred_json(entries)
+	results = entries.map do |el|
+		{
+			title: el[:char],
+			subtitle: "#{el[:keywords]} · Enter to copy... #{el[:char]}",
+			arg: el[:char]
+		}
+	end
+
+	{items: results}
+end
+
+results = search_arrows(arrows, queries)
+alfred_json = generate_alfred_json(results)
+print JSON.pretty_generate(alfred_json)
